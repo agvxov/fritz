3d256cd7078c0e92c79b7108af2a92c4d389f844
anon <anon@anon.anon>
2026-01-13 01:11:50 +0100

    figure out why the Serbian-code keeps crashing

--- Makefile
+++ Makefile
@@ -4,3 +4,4 @@ arsenal:
 	bake arsenal/hw.c
 	bake arsenal/exit.c
 	cd arsenal/hibot/ && bake main.c
+	cd arsenal/hibot/ && bake test.c
---
+++ arsenal/hibot/test.c
@@ -0,0 +1,16 @@
+// @BAKE gcc -o $*.out $@ -Wall -Wpedantic -ggdb
+#include <stdio.h>
+#include <string.h>
+#include <assert.h>
+#include "syntax.h"
+
+const char input[] = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+
+int main(void) {
+	assert(sizeof(input) == 512);
+
+	syntax_c();
+	puts(syntax_highlight(input));
+
+	return 0;
+}

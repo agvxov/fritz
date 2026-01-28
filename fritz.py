cede512a377b6342206d5b43325836038189d08f
anon <anon@anon.anon>
2026-01-28 19:25:36 +0100

    much better polling

--- fritz.py
+++ fritz.py
@@ -1,20 +1,16 @@
-# XXX: TODO polling event
-
 import ssl
+import heapq
 import socket
 import ctypes
 import atexit
 import signal
 import traceback
+from dataclasses import dataclass, field
 from os import fork, execvp, path, unlink, setpgid, kill
-#from sys import exit
 from time import time, sleep, monotonic
 from fcgi_client import FastCGIClient
 from irc.client import SimpleIRCClient, ServerConnectionError
 from irc.connection import Factory
-
-# Controls the poll period. In seconds.
-poll_tick = 1
 
 # --- ------- ---
 # --- Private ---
@@ -22,13 +18,30 @@ poll_tick = 1
 
 arms = []
 event_queues = {
-	'poll'	   : [],
+	'connect'  : [],
 	'join'	   : [],
 	'part'	   : [],
 	'quit'	   : [],
 	'priv_msg' : [],
 	'chan_msg' : [],
 }
+
+# Poll scheduling
+@dataclass(order=True)
+class PollingTask:
+	next_run : float
+	interval : float
+	arm      : object = field(compare=False)
+
+poll_scheduling_heap = []
+
+def add_polling(interval, arm):
+	now = monotonic()
+	heapq.heappush(poll_scheduling_heap, PollingTask(
+		next_run=now+interval,
+		interval=interval,
+		arm=arm,
+	))
 
 # FCGI process wrapper
 class Fritz_arm:
@@ -38,7 +51,7 @@ class Fritz_arm:
 			if path.exists(sock_path): return True
 			sleep(interval)
 		return False
-	def __init__(self, name : str) -> None:
+	def __init__(self, name : str):
 		self.name	   = name
 		self.sock_path = name.replace('.', '-') + ".sock"
 		self.pid = fork()
@@ -68,10 +81,10 @@ def handle_event(event : str, data : {}) -> str | None:
 		message = data["MESSAGE"].encode()
 		del data["MESSAGE"] # remove from passed env
 
-	for b in event_queues[event]:
-		response = b.client.request(data, message)
+	for arm in event_queues[event]:
+		response = arm.request(data, message)
 		if response is None:
-			print(f"!! 'None' response recieved from '{b.name}'")
+			print(f"!! 'None' response recieved from '{arm.name}'")
 			continue
 		r = response.decode(errors='ignore').strip()
 		print("<< " + r)
@@ -79,29 +92,29 @@ def handle_event(event : str, data : {}) -> str | None:
 
 	yield None
 
-
 # --- ------ ---
 # --- Public ---
 # --- ------ ---
 
-def add_arm(name, events):
+def add_arm(name, events=[], poll_interval=None):
 	arm = Fritz_arm(name)
 	arms.append(arm)
 	for event in events:
 		event_queues[event].append(arm)
+	if poll_interval != None:
+		add_polling(poll_interval, arm)
 
 class Fritz(SimpleIRCClient):
 	def __init__(self, server, nick, auto_join_list, port=6697, is_ssl=True):
 		def ssl_wrapper(sock):
 			context = ssl.create_default_context()
-			return context.wrap_socket(sock, server_hostname="chud.cyou")
+			return context.wrap_socket(sock, server_hostname=server)
 
 		atexit.register(my_atexit)
 
-		self.connection_time = 0
-		self.nick			 = nick
-		self.auto_join_list  = auto_join_list
-		self.joined			 = []
+		self.nick			= nick
+		self.auto_join_list = auto_join_list
+		self.joined			= []
 
 		if port < 6690:
 			is_ssl = False
@@ -122,50 +135,63 @@ class Fritz(SimpleIRCClient):
 
 		self.run()
 
-	def run(self):
-		next_poll = 0
-
-		while self.connection.is_connected():
-			self.reactor.process_once(timeout=.2)
-
-			now = time()
-			if now >= next_poll:
-				self.run_event_handler("poll", {})
-				next_poll = now + poll_tick
-
-	def run_event_handler(self, event_name: str, data: {}):
-		if event_name != "poll":
-			print(f">> {event_name}: {{{data}}}")
-
+	def insert_global_data(self, data : {str : str}, event_name : str) -> {str : str}:
 		data["JOINED"] = ":".join(self.joined)
 		data["EVENT"]  = event_name
+		return data
+
+	def handle_response(self, response : str | None):
+		print(response)
+		if response is None: return
+		if response == "": return
+		try:
+			lines	 = response.splitlines()
+			metadata = lines[0].strip()
+			target	 = metadata
+			body	 = lines[1:]
+		except:
+			print("!! Invalid response.")
+			return
+		if metadata == "!raw":
+			for l in body:
+				self.connection.send_raw(l)
+			return
+		if metadata == "!exit":
+			exit(0)
+		for l in body:
+			self.connection.privmsg(target, l)
+
+	def run_irc_event_handler(self, event_name: str, data: {}):
+		print(f">> {event_name}: {{{data}}}")
+
+		data = self.insert_global_data(data, event_name)
 
 		for response in handle_event(event_name, data):
-			if response is None: continue
-			if response == "": continue
-			try:
-				lines	 = response.splitlines()
-				metadata = lines[0].strip()
-				target	 = metadata
-				body	 = lines[1:]
-			except:
-				print("!! Invalid response.")
-				continue
-			if metadata == "!raw":
-				for l in body:
-					self.connection.send_raw(l)
-				return
-			if metadata == "!exit":
-				exit(0)
-			for l in body:
-				self.connection.privmsg(target, l)
+			self.handle_response(response)
 
-	def on_welcome(self, connection, event):
-		self.connection_time = time()
-		for chan in self.auto_join_list: connection.join(chan)
-		print(f"-- Fritz online @ {connection.server_address}")
+	def do_poll(self):
+		now = monotonic()
+		if poll_scheduling_heap and poll_scheduling_heap[0].next_run <= now:
+			task = heapq.heappop(poll_scheduling_heap)
+			print(f">> poll: {{{task.arm.name}}}")
+			task.next_run = now + task.interval
+			heapq.heappush(poll_scheduling_heap, task)
+			data = self.insert_global_data({}, 'poll')
+			response = task.arm.request(data, '')
+			if response != None: response = response.decode(errors='ignore').strip()
+			self.handle_response(response)
+
+	def run(self):
+		while self.connection.is_connected():
+			self.reactor.process_once(timeout=.2)
+			self.do_poll()
 
 	# === Common event handlers ===
+
+	def on_welcome(self, connection, event):
+		for chan in self.auto_join_list: connection.join(chan)
+		print(f"-- Fritz online @ {connection.server_address}")
+		self.run_irc_event_handler("connect", {})
 
 	def on_join(self, connection, event):
 		if event.source.nick == self.nick:
@@ -174,7 +200,7 @@ class Fritz(SimpleIRCClient):
 			"USERNAME" : event.source.nick,
 			"CHANNEL"  : event.target,
 		}
-		self.run_event_handler("join", records)
+		self.run_irc_event_handler("join", records)
 
 	def on_pubmsg(self, connection, event):
 		records = {
@@ -182,7 +208,7 @@ class Fritz(SimpleIRCClient):
 			"CHANNEL"  : event.target,
 			"MESSAGE"  : event.arguments[0],
 		}
-		self.run_event_handler("chan_msg", records)
+		self.run_irc_event_handler("chan_msg", records)
 
 	def on_privmsg(self, connection, event):
 		records = {
@@ -190,18 +216,18 @@ class Fritz(SimpleIRCClient):
 			"CHANNEL"  : event.source.nick,
 			"MESSAGE"  : event.arguments[0],
 		}
-		self.run_event_handler("priv_msg", records)
+		self.run_irc_event_handler("priv_msg", records)
 
 	def on_part(self, connection, event):
 		records = {
 			"USERNAME" : event.source.nick,
 			"CHANNEL"  : event.target,
 		}
-		self.run_event_handler("part", records)
+		self.run_irc_event_handler("part", records)
 
 	def on_quit(self, connection, event):
 		# NOTE: does NOT fire when executing QUIT, but disconnect does
 		records = {
 			"MESSAGE" : event.arguments[0] if event.arguments else "",
 		}
-		self.run_event_handler("quit", records)
+		self.run_irc_event_handler("quit", records)

faf57d9828f4b1c1fecb04ab74e6e4fae44c501b
anon <anon@anon.anon>
2026-01-30 14:09:55 +0100

    caught a bug

--- script/min-bot.py
+++ script/min-bot.py
@@ -1,25 +1,20 @@
 #!/bin/python3
+import time
 from irc.client import SimpleIRCClient, ServerConnectionError
 from irc.connection import Factory
 
 class MinBot(SimpleIRCClient):
-	def __init__(self, server, nick, auto_join_list, port, is_ssl):
-		def ssl_wrapper(sock):
-			context = ssl.create_default_context()
-			return context.wrap_socket(sock, server_hostname=server)
-
+	def __init__(self, server, nick, auto_join_list, port):
 		self.nick			= nick
 		self.auto_join_list = auto_join_list
-
-		factory = Factory(wrapper=ssl_wrapper) if is_ssl else Factory()
+		self.message_queue  = []
 
 		super().__init__()
 		try:
 			super().connect(
 				server,
 				port,
-				nick,
-				connect_factory = factory
+				nick
 			)
 		except Exception as e:
 			print(f"!! Failed to establish connection: '{e}'.")
@@ -27,17 +22,43 @@ class MinBot(SimpleIRCClient):
 
 		self.run()
 
+	def privmsg(self, target : str, message : str):
+		n_free_bytes = 112 - len('PRIVMSG  :\r\n') - len(target.encode('utf-8'))
+		raw_message = message.encode('utf-8')
+		for m in [raw_message[i:i+n_free_bytes] for i in range(0, len(raw_message), n_free_bytes)]:
+			self.message_queue.append((target, m.decode('utf-8', errors='ignore')))
+			mm = m.decode('utf-8', errors='ignore')
+			print(len(f"PRIVMSG {target} :{mm}\r\n".encode()))
+
+	def privmsg_frfr(self):
+		if len(self.message_queue) == 0: return
+		now = time.monotonic()
+		print(now, self.message_queue[0])
+		message = self.message_queue.pop(0)
+		self.connection.privmsg(*message)
+
+	#def run(self):
+	#	while self.connection.is_connected():
+	#		self.reactor.process_once(timeout=.2)
+
 	def run(self):
+		now = time.monotonic()
+		last_message = now - 1
 		while self.connection.is_connected():
 			self.reactor.process_once(timeout=.2)
+			now = time.monotonic()
+			if now - last_message > 1:
+				last_message = now
+				self.privmsg_frfr()
 
 	def on_welcome(self, connection, event):
-		for chan in self.auto_join_list: connection.join(chan)
+		for chan in self.auto_join_list:
+			connection.join(chan)
+			self.privmsg(chan, "ab" * 2024)
 
 MinBot(
 	"chud.cyou",
-	"MiBot",
+	"MinBot",
 	["#fritz"],
-	6667,
-	False
+	6667
 )
